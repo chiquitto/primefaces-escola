7ea10aab811953af47e0ae540c9b177f55cdbc12
Alisson Chiquitto <chiquitto@gmail.com>
2016-06-21 22:07:14 -0300

    Import inicial

---
+++ src/java/principal/ProfessorCadastrar.java
@@ -0,0 +1,38 @@
+package principal;
+
+import br.com.chiquitto.escola.Conexao;
+import br.com.chiquitto.escola.dao.ProfessorDao;
+import br.com.chiquitto.escola.vo.Professor;
+import java.util.Calendar;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class ProfessorCadastrar {
+    private Professor professor;
+
+    public ProfessorCadastrar() {
+        professor = new Professor();
+        
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.YEAR, -18);
+        professor.setNascimento(cal.getTime());
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+    
+    public void salvar() {
+        Conexao.setUrl("jdbc:sqlite:/Users/chiquitto/work/aula/java-caso-uso-escola/data/escola.sqlite.db");
+        ProfessorDao dao = new ProfessorDao();
+        dao.cadastrar(this.professor);
+    }
+    
+    
+}
